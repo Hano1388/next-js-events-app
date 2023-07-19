07ca29c1ebcdb910271a5092cfec2710a71f2533
Hano1388 <hinsul3113@gmail.com>
2023-07-18 18:27:48 -0700

    setup routes

---
+++ pages/events/[...eventFilters].js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>List of Filtered Events Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Events HomePage</h1>
+    </div>
+  );
+};
+
+export default HomePage;
